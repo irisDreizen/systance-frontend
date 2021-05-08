0a8cd2644e59b3f2ef447ed1e9fe09bd48588448
iris dreizenshtok <irisdrei7@gmail.com>
2021-05-08 21:04:14 +0300

    add emil page

---
+++ src/pages/emailPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div class="centered_content">
+            <div class="content_title">
+                Thanks for using our system!
+            </div>
+            <br>
+            <span>
+            We will send you an email as soon as our system will finish running your algorithms
+        </span>
+            <br>
+
+        </div>
+        <div class="main-header" style="position: relative">
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "emailPage"
+    }
+</script>
+
+<style scoped>
+    .content_title {
+        font-family: 'Bradley Hand', cursive;
+        font-weight: bold;
+        font-size: 50px;
+    }
+    .centered_content{
+        margin: 50px;
+        text-align: center;
+    }
+    .main-header {
+        width: 100%;
+        height: 200px;
+        background: url("../assets/thankYou.jpg");
+        background-size: cover;
+    }
+</style>
